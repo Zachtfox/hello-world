0c4d11ecf091fbd90d0a8d2ac285a6df3eb22ba7
Zach <zfox@students.mchenry.edu>
2018-08-23 10:35:25 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+        String aString = "Zach";
+        System.out.println("your name is " + aString);
+    }
+}
